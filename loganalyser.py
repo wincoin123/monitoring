e3dfa91aa50fcc9d8a0870375469def7b5154c00
wincoin123 <47448289+wincoin123@users.noreply.github.com>
2019-02-09 11:53:34 +0530

    extract  ip and time

--- loganalyser.py
+++ loganalyser.py
@@ -1,32 +1,25 @@
 import sys
 import re
 import datetime
-
+import time
+import json
+import math
 logfile = "D:/test_log.txt"
 
 file = open(logfile, "r")
-ips = {}
-
+ips = []
+data = {}
 strptime = datetime.datetime.strptime
 for text in file.readlines():
     text = text.rstrip()
     found = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})',text)
     match = text.partition('[')[-1].rpartition(']')[0]
     if found and match:
-        ips[match]=found            
-for ip in ips:
-    outfile = open("D:/updates.txt", "a")
-    attacker = "".join(ips[ip][0])
-    if len(ips[ip]) > 1:
-        server = "".join(ips[ip][1])
-    if attacker is not '':
-        print ("Request from: %s" % (attacker))
-        outfile.write(attacker)
-        outfile.write("@")
-        outfile.write(ip)
-        attacker = ''
-        if server is not '':
-            print (" to : %s" % (server))
-            server=''
-        print (" @ %s" % (ip))
-        outfile.write("\n")
+        s = match.split()
+        d=time.mktime(datetime.datetime.strptime(s[0], "%d/%b/%Y:%H:%M:%S").timetuple())
+        ips.append({'ipAddress': found[0], 'timestamp': math.ceil(d)})
+        
+print(ips)           
+
+json_data = json.dumps(data)
+print (json_data)
